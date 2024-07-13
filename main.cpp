2ef53ae2f535c708beff67912bdfb7402cf5b73d
kamiji-tomoyuki <k023g0115@g.neec.ac.jp>
2024-07-13 22:25:53 +0900

    リークチェック

--- main.cpp
+++ main.cpp
@@ -5,22 +5,10 @@
 #include "Input/WinApp.h"
 #include "Input/Input.h"
 #include "Input/DirectXCommon.h"
+#include "Input/D3DResourceLeakChecker.h"
 
 #pragma comment(lib, "dxguid.lib")
 #pragma comment(lib, "dxcompiler.lib")
-
-struct D3DResourceLeakChecker {
-	~D3DResourceLeakChecker()
-	{
-		// リリースリークチェック
-		Microsoft::WRL::ComPtr < IDXGIDebug1> debug;
-		if (SUCCEEDED(DXGIGetDebugInterface1(0, IID_PPV_ARGS(&debug)))) {
-			debug->ReportLiveObjects(DXGI_DEBUG_ALL, DXGI_DEBUG_RLO_ALL);
-			debug->ReportLiveObjects(DXGI_DEBUG_APP, DXGI_DEBUG_RLO_ALL); // リリース不足
-			debug->ReportLiveObjects(DXGI_DEBUG_D3D12, DXGI_DEBUG_RLO_ALL);
-		}
-	}
-};
 
 ///////////////////////////////////////////////////////////
 
