32be5f7fe45d4b1689fd56e1f0a58351540eac78
kamiji-tomoyuki <k023g0115@g.neec.ac.jp>
2024-10-02 12:00:41 +0900

    temp

--- Input/Sprite.h
+++ Input/Sprite.h
@@ -1,4 +1,17 @@
 #pragma once
+#pragma once
+#include <wrl.h>
+#include <numbers>
+
+#include "SpriteCommon.h"
+
+#include "../Math/Vector2.h"
+#include "../Math/Vector3.h"
+#include "../Math/Vector4.h"
+#include "../Math/Matrix4x4.h"
+#include "../Math/CalculateMath.h"
+
+
 //スプライト
 class Sprite
 {
